1ecb00ac8da410715f6e52f704fc954e932b4144
Your Name <you@example.com>
2023-09-16 13:46:50 +0700

    fix auth slow, add slider

--- pages/api/auth/oriauth.js
+++ pages/api/auth/oriauth.js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials"
-import jwt from "jsonwebtoken";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -24,30 +23,43 @@ export const authOptions = {
       },
       
       async authorize(credentials, req) {
-        if(credentials.password=="123"){
-          
-          var JWT_SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-          let token_data ={"sub": "123"}
-          let expires_delta=99999
-          const token = jwt.sign(token_data, JWT_SECRET_KEY);
-          let data = {
-            "status": "ok",
-            "message": "Logged in",
-            "accessToken": token,
-            "token_type": token,
-            "expiresIn": 5999940,
-            "user": {
-              "id": 1,
-              "fname": "123",
-              "access_token": token,
-              "username": "123",
-              "email": "123",
-              "avatar": "https://www.melivecode.com/users/1.png"
-            }
-          }
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        // var datatata = {
+        //   'username': '123',
+        //   'password': '321'
+        // }
+        
+        // const todoObject = {
+        //   username: 123,
+        //   password: '321'
+        // };
+        // const res = await fetch("http://127.0.0.1:8000/token", {
+          // process.env.API_URL
+
+          // แก้ตรงนี้เอา fetch API ออกเก็บไว้ใช้หลังจากยอมเสียเงินแล้ว ใช้ fix code ไปก่อนจะได้ไวๆ
+        const res = await fetch(
+          // process.env.NEXT_PUBLIC_API_URL+"token"
+          "https://fastapi-booking.vercel.app/token"
+          // "http://localhost:8000/token"
+          , {
+          method: 'POST',
+          // body: datatata,
+          // body: JSON.stringify(credentials),
+          // body: datatata,
+          body: JSON.stringify(credentials),
+          // headers: { "Content-Type": "application/x-www-form-urlencoded" }  
+          headers: { "Content-Type": "application/json" }
+           
+        })
+        const data = await res.json()
+        if (data.status == 'ok'){
           return data.user
         }
-        
         // Return null if user data could not be retrieved
         return null
       }
