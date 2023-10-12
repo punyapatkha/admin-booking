7c64e318de9b34fd4bf8be79d4107baa7f4d53f5
Your Name <you@example.com>
2023-10-12 23:23:27 +0700

    disuse chart.js use react-chartjs-2

--- pages/indexgraph.js
+++ pages/indexgraph.js
@@ -1,44 +1,36 @@
 
-
 import { Fragment, useState ,useEffect } from 'react'
-import { Chart } from "chart.js";
+import { Chart } from "chart.js/auto";
+
+import {Bar} from 'react-chartjs-2';
+
+
 export default function Home() {
-  
-  useEffect(() => {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-        datasets: [{
-          data: [86, 114, 106, 106, 107, 111, 133],
-          label: "Applied",
-          borderColor: "#3e95cd",
-          backgroundColor: "#7bb6dd",
-          fill: false,
-        }, {
-          data: [70, 90, 44, 60, 83, 90, 100],
-          label: "Accepted",
-          borderColor: "#3cba9f",
-          backgroundColor: "#71d1bd",
-          fill: false,
-        }, {
-          data: [10, 21, 60, 44, 17, 21, 17],
-          label: "Pending",
-          borderColor: "#ffa500",
-          backgroundColor: "#ffc04d",
-          fill: false,
-        }, {
-          data: [6, 3, 2, 2, 7, 0, 16],
-          label: "Rejected",
-          borderColor: "#c45850",
-          backgroundColor: "#d78f89",
-          fill: false,
-        }
-        ]
-      },
-    });
-  }, [])
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  }
+
  
   return (
     <>
@@ -46,8 +38,15 @@ export default function Home() {
     hello  {/* line chart */}
       <h1 className="w-auto mx-auto mt-10 text-xl font-semibold capitalize ">line Chart</h1>
       <div className="w-auto h-screen flex mx-auto my-auto">
-        <div className='border border-gray-400 pt-0 rounded-xl  w-full h-fit my-auto  shadow-xl'>
-          <canvas id='myChart'></canvas>
+        <div className='border border-gray-400 pt-0 rounded-xl  w-1/2 h-1/2 my-auto  shadow-xl bg-gray-100'>
+        <Bar
+          data={data}
+          // width={400}
+          // height={200}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
         </div>
       </div>
     </>
